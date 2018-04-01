5ef531a1667ae269f91680174f3ec0c0c29c716e
Abhijitno1 <abhijitno1@yahoo.com>
2018-04-01 17:10:47 +0530

    Routing implemented for welcome and product list page

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarRatingComponent } from './shared/widgets/star-rating.component';
@@ -10,10 +12,17 @@ import { ProductsService } from './products/products.service';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: 'products', component: ProductListComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full' }, //No path specified
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full' } //Nonexisting path specified
+    ])
   ],
   declarations: [ 
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarRatingComponent
